2712e45cbe3a2f8585376b0b69eb5e40c5b34d0c
NBprojekt <bartko.norbert@gmail.com>
2017-02-22 08:50:40 +0100

    DAC Upload

---
+++ DigitalAnalogConverter/src/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rainbowdash
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declare und start the main-form
+        FormMain startForm = new FormMain();
+        startForm.setVisible(true);
+    }
+    
+}
