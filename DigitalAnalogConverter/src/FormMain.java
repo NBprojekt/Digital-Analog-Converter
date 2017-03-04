94b3d86d5673e1649673b04d8810e64dc7a64145
NBprojekt <bartko.norbert@gmail.com>
2017-03-04 10:01:02 +0100

    Hinzufügen der Lizenz

--- DigitalAnalogConverter/src/FormMain.java
+++ DigitalAnalogConverter/src/FormMain.java
@@ -2,14 +2,25 @@
 import javax.swing.JOptionPane;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2017  Norbert Bartko
+ *
+ * Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder neueren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ * 
+ * Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+ * OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License für weitere Details.
+ * 
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
  */
 
 /**
  *
- * @author rainbowdash
+ * @author Norbert Bartko
  */
 public class FormMain extends javax.swing.JFrame {
 
